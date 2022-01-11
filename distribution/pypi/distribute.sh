0f0687bee322c06331b1394941af9af34a7d149c
Thomas Oettli <spacefreak@noop.ch>
2022-01-11 22:24:44 +0100

    fix pypi/distribute.sh

--- distribution/pypi/distribute.sh
+++ distribution/pypi/distribute.sh
@@ -7,9 +7,10 @@ script_dir=$(dirname "$(readlink -f -- "$BASH_SOURCE")")
 pkg_dir=$(realpath "${script_dir}/../..")
 
 cd "${pkg_dir}/dist"
+ls -la
 msg="Select version to distribute (cancel with CTRL+C):"
 echo "${msg}"
-select version in $(find . -maxdepth 1 -type f -name "pyinotifyd-*.*.*.tar.gz" -printf "%f\n" | sed "s#\.tar\.gz##g"); do
+select version in $(find . -maxdepth 1 -type f -name "pyquarantine-*.*.*.tar.gz" -printf "%f\n" | sed "s#\.tar\.gz##g"); do
   [ -n "${version}" ] && break
   echo -e "\ninvalid choice\n\n${msg}"
 done
